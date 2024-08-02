7da77a97c3dcb1a29367b6f00c8619add839efb2
ChillBroh <isharamadusanka410@gmail.com>
2024-08-02 15:59:54 +0530

    added readme and stack.ts

--- api-auth-jwt-cognito-user-pool/stacks/ExampleStack.ts
+++ api-auth-jwt-cognito-user-pool/stacks/ExampleStack.ts
@@ -1,5 +1,53 @@
-import { StackContext } from "sst/constructs";
+import { Api, Cognito, StackContext, StaticSite } from "sst/constructs";
 
-export function ExampleStack({ stack }: StackContext) {
-  // Add your first construct
+export function ExampleStack({ stack, app }: StackContext) {
+  // Create User Pool
+  const auth = new Cognito(stack, "Auth", {
+    login: ["email"],
+  });
+
+  // Create Api
+  const api = new Api(stack, "Api", {
+    authorizers: {
+      jwt: {
+        type: "user_pool",
+        userPool: {
+          id: auth.userPoolId,
+          clientIds: [auth.userPoolClientId],
+        },
+      },
+    },
+    defaults: {
+      authorizer: "jwt",
+    },
+    routes: {
+      "GET /private": "functions/private.main",
+      "GET /public": {
+        function: "functions/public.main",
+        authorizer: "none",
+      },
+    },
+  });
+
+  // allowing authenticated users to access API
+  auth.attachPermissionsForAuthUsers(stack, [api]);
+
+  // Show the API endpoint and other info in the output
+  const site = new StaticSite(stack, "Site", {
+    path: "frontend",
+    environment: {
+      VITE_APP_API_URL: api.url,
+      VITE_APP_REGION: app.region,
+      VITE_APP_USER_POOL_ID: auth.userPoolId,
+      VITE_APP_USER_POOL_CLIENT_ID: auth.userPoolClientId,
+    },
+  });
+
+  // Show the API endpoint and other info in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+    UserPoolId: auth.userPoolId,
+    UserPoolClientId: auth.userPoolClientId,
+    SiteUrl: site.url,
+  });
 }
